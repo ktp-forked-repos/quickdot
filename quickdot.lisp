d3afa46eaf8452be52cf65a6fbf2c8c8ab5fa43f
pablovaldes <p.valdes334@gmail.com>
2016-11-07 17:42:09 +0100

    Update quickdot.lisp

--- quickdot.lisp
+++ quickdot.lisp
@@ -1,7 +1,6 @@
 (defun quickdot ()
   "Creates a graphviz file (quick.dot) with the relationships among quicklisp systems. To obtain a pdf file use: sfdp -Goverlap=prism -Tpdf quick.dot > quick.pdf. You will need to have graphviz installed"
-  (with-open-file (gr (merge-pathnames (make-pathname :directory '(:relative 
-		   "quickdot") :name "quick" :type "dot") (user-homedir-pathname))
+  (with-open-file (gr (merge-pathnames (make-pathname :name "quick" :type "dot") (user-homedir-pathname))
 		 :direction :output
 		 :if-exists :rename
 		 :if-does-not-exist :create)
