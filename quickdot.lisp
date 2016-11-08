f6c624b992048947b01ad8241abe7fb020144f72
pablovaldes <p.valdes334@gmail.com>
2016-11-08 16:37:08 +0100

    Rewritten
    Produces a much more compact code, reduced around 50% with respect to the first version.

--- quickdot.lisp
+++ quickdot.lisp
@@ -2,14 +2,18 @@
   "Creates a graphviz file (quick.dot) with the relationships among quicklisp systems. To obtain a pdf file use: sfdp -Goverlap=prism -Tpdf quick.dot > quick.pdf. You will need to have graphviz installed"
   (with-open-file (gr (merge-pathnames (make-pathname :name "quick" :type "dot") (user-homedir-pathname))
 		 :direction :output
-		 :if-exists :rename
+		 :if-exists :supersede
 		 :if-does-not-exist :create)
+;(let *color* '(255 255 255)
     (loop
-       initially (format gr "digraph quicklisp \{~%rankdir=LR;~%edge [arrowhead\=\"none\"];~%~%" )
+       initially (format gr "graph quicklisp \{~%rankdir=LR;~%edge [arrowhead\=\"none\"];~%~%")
        for i in (ql:system-list) do
 	 (format gr "\"~a\" [style\=\"filled\", fillcolor\=\"gray\", label\=\"~a\"];~%"
-		 (ql-dist:name i) (ql-dist:name i))
-	 (loop for req in (ql-dist:required-systems i) do
-	      (format gr "\"~a\" -> \"~a\";~%"
-		      (ql-dist:name i) req))
-       finally (format gr "}"))))
+		 (ql-dist:name i)
+		 (ql-dist:name i))
+
+	 (if (> (length (ql-dist:required-systems i)) 0) 
+	 (format gr "\"~a\" -- \{~{\"\~a~^\" ~}\"\};~%"
+		 (ql-dist:name i)
+		 (ql-dist:required-systems i)))
+        finally (format gr "}"))))
